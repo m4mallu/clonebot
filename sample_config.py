cadf580130ba08bb69940c39b36934c3eded3ec7
Rinrinx2 <58888200+rinrinx2@users.noreply.github.com>
2022-10-17 11:48:47 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,3 @@
-#----------------------------------- https://github.com/m4mallu/clonebot --------------------------------------------#
 import os
 import logging
 
@@ -11,21 +10,11 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
 
-    # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-
-    # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
-
-    # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
-
-    # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
-
-    # Database URI
-    DB_URI = os.environ.get("DATABASE_URL", "")
-
+    TG_BOT_TOKEN = ""
+    APP_ID = ""
+    API_HASH = ""
+    TG_USER_SESSION = ""
+    DB_URI = ""
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
